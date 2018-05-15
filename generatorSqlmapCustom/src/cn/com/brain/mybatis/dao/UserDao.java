20562a3f5ab396eeeb33992a3eb5b013b682b09b
邸一行 <2200204568@.com>
2018-05-15 16:11:19 +0800

    update

---
+++ generatorSqlmapCustom/src/cn/com/brain/mybatis/dao/UserDao.java
@@ -0,0 +1,5 @@
+package cn.com.brain.mybatis.dao;
+
+public interface UserDao {
+
+}
---
+++ generatorSqlmapCustom/src/cn/com/brain/mybatis/dao/UserDaoImpl.java
@@ -0,0 +1,12 @@
+package cn.com.brain.mybatis.dao;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+public class UserDaoImpl extends SqlSessionDaoSupport implements UserDao  {
+
+	
+
+	public void inserUser(){
+		//this.getSqlSession().insert(arg0,arg1);
+	}
+}
